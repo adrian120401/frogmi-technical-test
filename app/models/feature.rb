b2b8979416e6ce7ba38aaef3978df33c07422878
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2024-04-11 00:23:47 -0300

    build: init project, task and get controller

---
+++ app/models/feature.rb
@@ -0,0 +1,18 @@
+class Feature
+    include Mongoid::Document
+    field :mag, type: Float
+    field :place, type: String
+    field :time, type: DateTime
+    field :url, type: String
+    field :title, type: String
+    field :tsunami, type: Boolean
+    field :mag_type, type: String
+    field :longitude, type: Float
+    field :latitude, type: Float
+
+    validates :title, :url, :place, :mag_type, :longitude, :latitude, presence: true
+    validates :mag, numericality: { greater_than_or_equal_to: -1.0, less_than_or_equal_to: 10.0 }
+    validates :latitude, numericality: { greater_than_or_equal_to: -90.0, less_than_or_equal_to: 90.0 }
+    validates :longitude, numericality: { greater_than_or_equal_to: -180.0, less_than_or_equal_to: 180.0 }
+    validates :url, uniqueness: true
+end
